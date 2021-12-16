e031cbef2f12517d8d2ea79225a10942a614a79b
WassimKallel <wassimkallel@think-it.io>
2021-12-16 16:40:02 +0100

    feat: added logging

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const winston = require("winston");
 
-app.get('/', (req, res) => {
-  res.send('Hello World! v2')
-})
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  defaultMeta: { service: "hello-world-service" },
+  transports: [new winston.transports.Console()],
+});
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  logger.debug("connection on route /");
+  res.send("Hello World! v2");
+});
+
+app.get("/error", (req, res) => {
+  logger.error("connection on route /error");
+  res.send("error Logged");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  logger.info(`Example app listening at http://localhost:${port}`);
+});
